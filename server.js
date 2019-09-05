282cfea6602c7165043d6c289af27caab41de792
medihamad <kumarpatel692@gmail.com>
2019-09-05 19:45:26 -0400

    adding first end point

---
+++ server.js
@@ -0,0 +1,19 @@
+const http = require('http');
+const express = require('express');
+
+const app = express();
+
+app.use(express.json());
+
+
+app.use('/', function(req, res) {
+    res.send('todo-api works');
+});
+
+const server = http.createServer(app);
+
+const port = 3000;
+
+server.listen(port);
+
+console.debug('Server listening on port ' + port);
