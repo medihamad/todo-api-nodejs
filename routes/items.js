e5f57f15f128f8bd74fa7f2c015f6bff3e5c9083
medihamad <kumarpatel692@gmail.com>
2019-09-07 01:43:38 -0400

    adding post, delete and update endpoints

--- routes/items.js
+++ routes/items.js
@@ -26,4 +26,63 @@ router.get('/:id', function(req, res) {
 });
 
 
+router.post('/', function(req, res) {
+    let itemIds = data.map(item => item.id);
+    let orderNums = data.map(item => item.order);
+
+    let newId = itemIds.length > 0 ? Math.apply(Math, itemIds) + 1 : 1;
+    let newOrderNum = orderNums.length > 0 ? Math.max.apply(Math, orderNums) + 1 : 1;
+
+    let newItem = {
+        id: newId, 
+        title: req.body.title,
+        order: newOrderNum,
+        completed: false,
+        createdOn: new Date()
+    };
+
+    data.push(newItem);
+
+    res.status(201).json(newItem);
+});
+
+
+router.put('/:id', function(req, res) {
+    let found = data.found(function (item) {
+        return item.id === parseInt(req.params.id);
+    })
+});
+
+if(found){
+    let updated = {
+        id: found.id,
+        title: req.body.title,
+        order: req.body.order,
+        completed: req.body.completed
+    };
+
+    let targetIndex = data.indexOf(found);
+
+    data.splice(targetIndex, 1, updated);
+
+    res.sendStatus(204);
+} else{
+    res.sendStatus(404);
+}
+
+
+router.delete('/:id', function(req, res) {
+    let found = data.find(function (item){
+        return item.id === parseInt(req.params.id);
+    });
+
+    if(found){
+        let targetIndex = data.indexOf(found);
+
+        data.splice(targetIndex, 1);
+    }
+
+    res.sendStatus(204);
+});
+
 module.exports = router;
